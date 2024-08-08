f2bb1db65546b389fddf3f7603871897662bdc38
Vadimk <vadimkotkin1855@gmail.com>
2024-08-08 17:51:00 +0200

    init commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    @Get() //GET /users
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+        return []
+    }
+
+    @Get(':id') //GET /users/:id
+    findOne(@Param('id') id: string){
+        return {id}
+    }
+
+    @Post() //POST /users
+    create(@Body() user: {}){
+        return user
+    }
+
+    @Patch(':id')
+    updateOne(@Param('id') id: string, @Body() userUpdate: {}){
+        return {id, ...userUpdate}
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id: string){
+        return {id}
+    }
+}
